56727459814ea5f2463ffc7377cdca98224df2ce
biagolini <c.biagolini@gmail.com.com>
2019-10-09 18:46:11 -0300

    Update threshold_color function. Main update: Inclusion of the possibility of users process single channel images.

--- R/cnvIM.R
+++ R/cnvIM.R
@@ -5,13 +5,21 @@
 #' @description Convert a single image into a matrix
 #' @param filename Name of the file to be load - ex: "Figure01.JPG".
 #' @param filetype Type of the file to be load. Compatible file types: ".JPGE", ".JPG" or ".PNG".
-#' @param compress_method For high resolution files, i.e. numbers of pixels in width and height, it is suggested to reduce the resolution to create a smaller matrix, it strongly reduce GPU usage and time necessary to run analyses. On the other hand, by reducing resolution, it will also reduce the accuracy of data description. Methods for image compression: (i) frame_fixed: compress images to a desired target width and height; (ii) proportional:  compress the image by a given ratio provide in the argument "proportion"; (iii) width_fixed: compress images to get a target width. The image height will also be reduced, in the same intensity. For instance, if the original file had 1000 pixel in width, and width_fixed was set to 100, the height reduction will reduce in a rate of 0.1 (100/1000) ; (iv) height_fixed: analogous to width_fixed, but assumes height as reference.
+#' @param compress_method For high resolution files, i.e. numbers of pixels in width and height, it is suggested to reduce the resolution to create a smaller matrix,
+#' it strongly reduce GPU usage and time necessary to run analyses. On the other hand, by reducing resolution, it will also reduce the accuracy of data description.
+#' Methods for image compression: (i) frame_fixed: compress images to a desired target width and height; (ii) proportional:  compress the image by a given ratio provide in the argument "proportion";
+#' (iii) width_fixed: compress images to get a target width. The image height will also be reduced, in the same intensity. For instance, if the original file had 1000 pixel in width, and
+#' width_fixed was set to 100, the height reduction will reduce in a rate of 0.1 (100/1000) ; (iv) height_fixed: analogous to width_fixed, but assumes height as reference.
 #' @param compress_rate Compress rate to by apply if compress_method=proportional. Note:  it should be ser as number range from 0 to 1 .
 #' @param target_width Target width to be used if compress_method=frame_fixed or compress_method= width_fixed.
 #' @param target_height Target height to be used if compress_method=frame_fixed or compress_method= height_fixed.
-#' @param black_regulation For each pixel, the intensity of red, green and blue is averaged and compared to a black_regulation (threshold).
-#' If the average intensity is less than the black_regulation (default is 0.5) the pixel will be set as black, otherwise it will be white.
-#' @param transparency_regulation For PNG images, the alpha channel is used to set transparent pixels, i.e. alpha channel values above transparency_regulation (a threshold) will set the pixel as transparent, default is 0.5.  NOTE: In the data matrix the value 1 represents black pixels, 0 represents white pixels and NA represents transparent pixels.
+#' @param threshold_value For each pixel, the intensity of color channels (red, green and blue) are averaged and compared to a threshold_value (threshold).
+#' If the average intensity is less than the threshold_value (default is 0.5) the pixel will be set as black, otherwise it will be white. See channel argument.
+#' @param transparency_regulation For PNG images, the alpha channel is used to set transparent pixels, i.e. alpha channel values above transparency_regulation (a threshold)
+#'  will set the pixel as transparent, default is 0.5.  NOTE: In the data matrix the value 1 represents black pixels, 0 represents white pixels and NA represents transparent pixels.
+#' @param channel RGB channel to be considered in threshold. If channel=RGB (default), the intensity of red, green and blue is averaged and compared to threshold_value.
+#' If the average intensity is less than the threshold_value (default is 50%) the pixel will be set as black (matrix cell value =1), otherwise it will be white (matrix cell value =0).
+#' If only one channel is defined ("R" for red, "G" for green, and "B" for blue), the average intensity selected channel compared direct to the threshold_value value.
 #' @return A matrix of 0, 1 and NA representing white, black and transparent pixels, respectively.
 #' @author Carlos Biagolini-Jr.
 #' @examples
@@ -25,7 +33,7 @@
 #' @export
 threshold_color <-
   function(filename, filetype = "jpeg", compress_method = "none", compress_rate = 1,
-           target_width = 100, target_height = 100, black_regulation = 0.5, transparency_regulation = 0.5) {
+           target_width = 100, target_height = 100, threshold_value = 0.5, transparency_regulation = 0.5,channel="rgb") {
     # Validando os dados
     if (filetype == "png"|filetype == "PNG") {
       requireNamespace("png")
@@ -39,8 +47,8 @@ threshold_color <-
     if (compress_rate <= 0 | compress_rate > 1) {
       stop("compress_rate must be a number between 0 and 1 ")
     }
-    if (black_regulation >= 1 | black_regulation < 0) {
-      stop("black_regulation must be a number between 0 and 1")
+    if (threshold_value > 1 | threshold_value < 0) {
+      stop("threshold_value must be a number between 0 and 1")
     }
     if (!(compress_method == "none" | compress_method == "frame_fixed" | compress_method == "proportional" |
           compress_method == "width_fixed" | compress_method == "height_fixed")) {
@@ -85,26 +93,32 @@ threshold_color <-
               espaco_entre_pixeis_linha <- nrow(imagematrix)/linhas_imagem  # espaco entre os pixeis das linhas na figura orginal
               espaco_entre_pixeis_coluna <- ncol(imagematrix)/colunas_imagem  # espaco entre os pixeis das colunas na figura orginal
             }}}}}
+
+    #Definir range das cores que serao utilizadas e maximo da soma
+    if(channel=="rgb"|channel=="RGB"){range<-1:3;max.soma<-3
+    }else{
+      if(channel=="r"|channel=="R"){range<-1;max.soma<-1
+      }else{
+        if(channel=="g"|channel=="G"){range<-2;max.soma<-1
+        }else{
+          if(channel=="b"|channel=="B"){range<-3;max.soma<-1
+          }else{ stop("Provide a valid channel")}}}}
+    # Cria uma matriz de responta e pinta as celulas
     matrix_cores <- matrix(NA, nrow = linhas_imagem, ncol = colunas_imagem)
     if (filetype == "jpeg" | filetype == "jpg") {
       for (c in 1:ncol(matrix_cores)) {
         for (l in 1:nrow(matrix_cores)) {
-          if (sum(imagematrix[floor(l * espaco_entre_pixeis_linha), floor(c * espaco_entre_pixeis_coluna),
-                              1:3]) < ((black_regulation * 1.5) + 1.5)) {
+          if ((sum(imagematrix[floor(l * espaco_entre_pixeis_linha), floor(c *espaco_entre_pixeis_coluna),range])/max.soma) < threshold_value ) {
             matrix_cores[l, c] <- 1
           } else {
             matrix_cores[l, c] <- 0}}}}
     if (filetype == "png") {
       for (c in 1:ncol(matrix_cores)) {
         for (l in 1:nrow(matrix_cores)) {
-          if (imagematrix[floor(l * espaco_entre_pixeis_linha), floor(c * espaco_entre_pixeis_coluna),
-                          4] < transparency_regulation) {
-            matrix_cores[l, c] <- NA
+          if (imagematrix[floor(l * espaco_entre_pixeis_linha), floor(c * espaco_entre_pixeis_coluna),4] < transparency_regulation) {matrix_cores[l, c] <- NA
           } else {
             # deixando as caixas
-            if (sum(imagematrix[floor(l * espaco_entre_pixeis_linha), floor(c * espaco_entre_pixeis_coluna),
-                                1:3]) < ((black_regulation * 1.5) + 1.5)) {
-              matrix_cores[l, c] <- 1
+            if ((sum(imagematrix[floor(l * espaco_entre_pixeis_linha), floor(c *espaco_entre_pixeis_coluna),range])/max.soma)<threshold_value) {matrix_cores[l, c] <- 1
             } else {
               matrix_cores[l, c] <- 0}}}}}
     return(matrix_cores)}
@@ -113,12 +127,21 @@ threshold_color <-
 #' @description Convert two or more images into a list of matrices
 #' @param list_names An object contains the names of the files.
 #' @param filetype Type of the file to be load. Compatible file types: ".JPGE", ".JPG" or ".PNG".
-#' @param compress_method For high resolution files, i.e. numbers of pixels in width and height, it is suggested to reduce the resolution to create a smaller matrix, it strongly reduce GPU usage and time necessary to run analyses. On the other hand, by reducing resolution, it will also reduce the accuracy of data description. Methods for image compression: (i) frame_fixed: compress images to a desired target width and height; (ii) proportional:  compress the image by a given ratio provide in the argument "proportion"; (iii) width_fixed: compress images to get a target width. The image height will also be reduced, in the same intensity. For instance, if the original file had 1000 pixel in width, and width_fixed was set to 100, the height reduction will reduce in a rate of 0.1 (100/1000) ; (iv) height_fixed: analogous to width_fixed, but assumes height as reference.
+#' @param compress_method For high resolution files, i.e. numbers of pixels in width and height, it is suggested to reduce the resolution to create a smaller matrix,
+#' it strongly reduce GPU usage and time necessary to run analyses. On the other hand, by reducing resolution, it will also reduce the accuracy of data description.
+#' Methods for image compression: (i) frame_fixed: compress images to a desired target width and height; (ii) proportional:  compress the image by a given ratio provide in the argument "proportion";
+#' (iii) width_fixed: compress images to get a target width. The image height will also be reduced, in the same intensity. For instance, if the original file had 1000 pixel in width, and
+#' width_fixed was set to 100, the height reduction will reduce in a rate of 0.1 (100/1000) ; (iv) height_fixed: analogous to width_fixed, but assumes height as reference.
 #' @param compress_rate Compress rate to by apply if compress_method=proportional. Note:  it should be ser as number range from 0 to 1 .
 #' @param target_width Target width to be used if compress_method=frame_fixed or compress_method= width_fixed.
 #' @param target_height Target height to be used if compress_method=frame_fixed or compress_method= height_fixed.
-#' @param black_regulation For each pixel, the intensity of red, green and blue is averaged and compared to a black_regulation (threshold). If the average intensity is less than the black_regulation (default is 0.5) the pixel will be set as black, otherwise it will be white.
-#' @param transparency_regulation For PNG images, the alpha channel is used to set transparent pixels, i.e. alpha channel values above transparency_regulation (a threshold) will set the pixel as transparent, default is 0.5.  NOTE: In the data matrix the value 1 represents black pixels, 0 represents white pixels and NA represents transparent pixels.
+#' @param threshold_value For each pixel, the intensity of color channels (red, green and blue) are averaged and compared to a threshold_value (threshold).
+#' If the average intensity is less than the threshold_value (default is 0.5) the pixel will be set as black, otherwise it will be white. See channel argument.
+#' @param transparency_regulation For PNG images, the alpha channel is used to set transparent pixels, i.e. alpha channel values above transparency_regulation (a threshold)
+#'  will set the pixel as transparent, default is 0.5.  NOTE: In the data matrix the value 1 represents black pixels, 0 represents white pixels and NA represents transparent pixels.
+#' @param channel RGB channel to be considered in threshold. If channel=RGB (default), the intensity of red, green and blue is averaged and compared to threshold_value.
+#' If the average intensity is less than the threshold_value (default is 50%) the pixel will be set as black (matrix cell value =1), otherwise it will be white (matrix cell value =0).
+#' If only one channel is defined ("R" for red, "G" for green, and "B" for blue), the average intensity selected channel compared direct to the threshold_value value.
 #' @return A matrix of 0, 1 and NA representing white, black and transparent pixels, respectively.
 #' @author Carlos Biagolini-Jr.
 #' @seealso threshold_color
@@ -132,20 +155,19 @@ threshold_color <-
 #' @export
 threshold_image_list <-
   function(list_names, filetype = "jpeg", compress_method = "none", compress_rate = 1,
-           target_width = 100, target_height = 100, black_regulation = 0.5, transparency_regulation = 0.5) {
+           target_width = 100, target_height = 100, threshold_value = 0.5, transparency_regulation = 0.5,channel="rgb") {
     pb <- txtProgressBar(min = 0, max = length(list_names), style = 3)
     lista_de_saida <- list(NA)
     for (i in 1:length(list_names)) {
       matrix_para_analise <- threshold_color(filename = list_names[i], filetype = filetype, compress_method = compress_method,
-                                             compress_rate = compress_rate, black_regulation = black_regulation, target_width = target_width,
-                                             target_height = target_height, transparency_regulation = transparency_regulation)
+                                             compress_rate = compress_rate, threshold_value = threshold_value, target_width = target_width,
+                                             target_height = target_height, transparency_regulation = transparency_regulation,channel=channel)
       lista_de_saida[[i]] <- matrix_para_analise
       setTxtProgressBar(pb, i)
     }
     close(pb)
     resposta <- lista_de_saida
     return(resposta)}
-
 #' @title stretch circle to square
 #'
 #' @description Stretch data data from circular image to square in binary matrix
